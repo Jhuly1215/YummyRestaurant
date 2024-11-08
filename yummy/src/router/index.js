5ef60e2ef10b82bb31a4d674ba694d97a028438a
Gaby B. <gabtbm2104@gmail.com>
2024-11-08 15:11:00 -0400

    router index.jx

--- yummy/src/router/index.js
+++ yummy/src/router/index.js
@@ -1,13 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import home from '@/ventanas/home.vue'
 import loginView from '../ventanas/LoginView.vue'
+import MenuCliente from '@/ventanas/MenuCliente.vue'
+import ofertasPage from '@/ventanas/ofertasPage.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
       {
+        path: '/',
+        name: 'home',
+        component: home
+      },
+      {
         path: '/login',
         name: 'login',
         component: loginView
+      },
+      {
+        path: '/menu',
+        name: 'menu',
+        component: MenuCliente
+      },
+      {
+        path: '/ofertas',
+        name: 'ofertas',
+        component: ofertasPage
       },
     ]
   })
