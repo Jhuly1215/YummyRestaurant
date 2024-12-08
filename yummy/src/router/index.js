b910557572c956591dde925716abf6c1cfc149bf
unknown <jhuliannnavitoria@gmail.com>
2024-12-08 12:01:05 -0400

    mis reservas done

--- yummy/src/router/index.js
+++ yummy/src/router/index.js
@@ -7,22 +7,17 @@ import PanelAdministrativo from '@/ventanas/panelAdministrativo.vue';
 import Recupera from '@/ventanas/recupera.vue';
 import CambioPass from '@/ventanas/cambioPassword.vue';
 import Ofertas from '@/ventanas/ofertasPage.vue';
-
-import MenuPedido from '@/ventanas/MenuPedido.vue';
-import PedidosAdmin from '@/ventanas/PedidosAdmin.vue';
-
-import MapaInteractivo1 from '@/ventanas/mapaInteractivo.vue';
-
+import MapaInteractivo from '@/ventanas/mapaInteractivo.vue';
 import MenuCliente from '@/ventanas/MenuCliente.vue'
 import TemporalCalificacion from '@/ventanas/temporalCalificacion.vue'
+import MisReservas from '@/ventanas/reservas/MisReservas.vue';
 
 //para el administrador
 
-import MapaInteractivo2 from '@/ventanas/mapaAdmin.vue';
+import Section1 from '../ventanas/Section2Page.vue';
 import OfertasAdminComponent from '../ventanas/OfertasAdminComponent.vue'
 import PlatillosAdminComponent from '@/components/PlatillosAdminComponent.vue';
 import JReservasAdminComponent from '@/ventanas/reservas/Reservas.vue';
-import DashboardComponent from '../ventanas/dashboardComponent.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -62,10 +57,10 @@ const router = createRouter({
       name: 'Menu',
       component: MenuCliente,
     },
-    {
-      path: '/recupera',
-      name: 'Recupera',
-      component: Recupera,
+      {
+        path: '/recupera',
+        name: 'Recupera',
+        component: Recupera,
     },
     {
       path: '/cambioPass/:id',
@@ -74,78 +69,62 @@ const router = createRouter({
     },
     {
       path: '/mapa',
-      name: 'MapaUsuario',
-      component: MapaInteractivo1,
+      name: 'Mapa',
+      component: MapaInteractivo,
     },
-    {
-      path: '/menucliente',
-      name: 'MenuCliente',
-      component: MenuCliente,
-    },
-    {
-      path: '/menupedido',
-      name: 'MenuPedido',
-      component: MenuPedido,
-    },
-    
     {
       path: '/temporal',
       name: 'Calificaciones',
       component: TemporalCalificacion,
     },
-
-   {
-    path: '/panelAdministrativo',
-    name: 'PanelAdministrativo',
-    component: PanelAdministrativo,
-    children: [
-      {
-        path: 'section1',
-        name: 'Dashboard',
-        component: DashboardComponent,
-      },
-      {
-        path: 'ofertas',
-        name: 'AdminOfertas',
-        component: OfertasAdminComponent,
-      },
-      {
-        path: 'platillos',
-        name: 'AdminPlatillos',
-        component: PlatillosAdminComponent,
-      },
-      {
-        path: 'reservas',
-        name: 'AdminReservas',
-        component: JReservasAdminComponent,
-      },
-      {
-        path: 'mapa',
-        name: 'MapaAdmin',
-        component: MapaInteractivo2,
-      },
-      {
-        path: 'pedidosadmin',
-        name: 'PedidosAdmin',
-        component: PedidosAdmin,
-      },
-    ],
-  },
+    {
+      path: '/misreservas',
+      name: 'MisReservas',
+      component: MisReservas,
+    },
+    //Para el Panel Administrativo
+    {
+      path: '/panelAdministrativo',
+      name: 'PanelAdministrativo',
+      component: PanelAdministrativo,
+      //meta: { requiresAuth: true }, // Requiere autenticación
+      children: [
+        {
+          path: 'section1', // Ruta base de panel administrativo
+          name: 'Dashboard',
+          component: Section1, // Componente del Dashboard
+        },
+        {
+          path: 'ofertas',
+          name: 'AdminOfertas',
+          component: OfertasAdminComponent, // Gestión de ofertas
+        },
+        {
+          path: 'platillos',
+          name: 'AdminPlatillos',
+          component: PlatillosAdminComponent, // Gestión de platillos
+        },
+        {
+          path: 'reservas',
+          name: 'AdminReservas',
+          component: JReservasAdminComponent, // Gestión de reservas
+        },
+      ],
+    },
   ],
 });
-router.beforeEach((to, from, next) => {
-  const token = localStorage.getItem('token');
-  const rol = parseInt(localStorage.getItem('rol'), 10);
 
-  if (to.name === 'PanelAdministrativo' && (!token || rol !== 2)) {
-    next('/iniciarsesion'); // Redirige al inicio de sesión si no es admin
-  } else if (to.name === 'Reservas' && !token) {
-    next('/iniciarsesion'); // Redirige al inicio de sesión si no está logueado
-  } else if (to.name === 'MapaAdmin' && (!token || rol !== 2)) {
-    next('/iniciarsesion'); // Protege el mapa del admin
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const token = localStorage.getItem('token'); 
+    if (!token) {
+      next({ name: 'LogIn' });
+    } else {
+      next();
+    }
   } else {
     next();
   }
 });
-
 export default router;
