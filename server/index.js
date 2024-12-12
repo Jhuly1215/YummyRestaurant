998286229a91cc7e0d3953be72c3d01cbdfa7226
KernelOS <dominicescalante22@gmail.com>
2024-12-12 11:21:14 -0400

    NO SE QUE ESTA PASANDO

--- server/index.js
+++
@@ -1,7 +0,0 @@
-// server/src/index.js
-const app = require('./app');
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
--- server/src/api/autenticacion/authRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const { login } = require('./authController');
-const router = express.Router();
-console.log("se cargan las rutas")
-router.post('/login', login);
-
-module.exports = router;
